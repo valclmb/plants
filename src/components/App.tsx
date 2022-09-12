5f0371a4707fbabeda9c53e10a6b409da1c67c7b
ValCLMB <valentin.coulomb2@gmail.com>
2022-09-12 10:38:35 +0200

    initialize react project and create the title and interface Plant.ts

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+export const App = () => {
+  return (
+    <div className="App">
+      <h1>Les plantes</h1>
+    </div>
+  );
+};
---
+++ src/models/Plant.ts
@@ -0,0 +1,3 @@
+export interface Plant {
+  name: string;
+}
