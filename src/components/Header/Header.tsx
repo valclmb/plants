2b9aa70f44e85be48b22f3073ffd4010cdd88413
ValCLMB <valentin.coulomb2@gmail.com>
2022-09-13 11:16:52 +0200

    Create nav (import fontawesome)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import "./Header.css";
-export const Header = () => {
-  return (
-    <header className="header">
-      <h1 className="header__title">Plants'</h1>
-      <p className="header__tagline">Achetez vos plantes en toute sérénité.</p>
-    </header>
-  );
-};
+import { Nav } from "./Nav/Nav";
+export const Header = () => (
+  <header className="header">
+    <div className="header__logo">
+      <h1 className="header__logo__title">Plants'</h1>
+      <p className="header__logo__tagline">
+        Achetez vos plantes en toute sérénité.
+      </p>
+    </div>
+    <Nav />
+  </header>
+);
